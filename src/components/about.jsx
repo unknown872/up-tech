33eddb9e0903ff6d9c50fbdf2589079422dbec38
unknown872 <paulpogboom87@gmail.com>
2024-07-10 16:11:20 +0000

    others change

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,12 +10,12 @@ function About() {
 
     return (
         <section id="about" className="py-20 bg-beige bg-cover">
-            <div className="flex gap-4 py-10" >
+            <div className="lg:flex lg:ml-0 gap-4 py-10 ml-4" >
                 <div className="w-1/2" data-aos="zoom-in">
-                    <Image src={banner} />
+                    <Image src={banner}/>
                 </div>
                 <div className="w-1/2">
-                    <h1 className='text-2xl text-blue-950 font-semibold' data-aos="fade-right" data-aos-duration="3000">Qui sommes-nous?</h1>
+                    <h1 className='text-2xl whitespace-nowrap text-blue-950 font-semibold' data-aos="fade-right" data-aos-duration="3000">Qui sommes-nous?</h1>
                     <p className='text-4xl mt-2 font-extrabold text-blue-900' data-aos="fade-right" data-aos-duration="3000">Le Partenaire stratégique <br /> de votre transformation <br /> digitale</p>
                     <p className='mt-4 text-xl font-semibold text-slate-700' data-aos="fade-right" data-aos-duration="3000">Nous adaptons notre prestation à la manière dont vous travaillez</p>
                     <p className='mt-4 text-lg font-normal text-slate-700' data-aos="fade-right" data-aos-duration="3000">Vous méritez des soins simples, personnalisés et sans tracas. Une <br /> assurance sûre conçue pour vous aider à prospérer.</p>
