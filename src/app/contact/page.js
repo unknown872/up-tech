41ecae22060e889389562aed9e69587497f2aafb
unknown872 <paulpogboom87@gmail.com>
2024-07-12 21:13:26 +0000

    others change

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,12 +1,152 @@
 import Contact from '@/components/contact'
+import Footer from '@/components/footer'
 import Header from '@/components/header'
 import React from 'react'
+import { MdArrowOutward } from "react-icons/md";
 
 function page() {
+    const contactMethods = [
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                </svg>
+            ,
+            contact: "+221 77 095 75 60",
+            title: "Phone"
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                </svg>
+            ,
+            contact: "uptech@gmail.com",
+            title: "Email"
+        },
+        {
+            icon:
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                </svg>
+            ,
+            contact: "Cite Khandar Ouest Foire. No 105 Dakar, Senegal",
+            title: "Our office"
+        },
+    ]
     return (
         <div>
             <Header />
-            <Contact />
+            <section>
+                <div className='p-20 bg-contact bg-cover bg-opacity-50 pt-44'>
+                    <h1 className='text-center text-6xl text-white font-bold'>Contact</h1>
+                </div>
+                <div className="lg:flex bg-beige lg:p-20 p-4 gap-x-14">
+                    <div className="lg:w-1/2">
+                        <div data-aos="fade-right" data-aos-duration="300" className='lg:mt-0 mt-8'>
+                            <h1 className="font-bold text-lg text-gray-600 animate-slidein">
+                                Restez-connecté
+                            </h1>
+                            <p className='mt-4 font-bold text-2xl text-blue-950'>
+                                Opter pour nos services, c‘est aborder le monde digital avec sérénité !
+                            </p>
+                        </div>
+                        <div>
+                            <ul className="mt-12 flex flex-col gap-x-12 gap-y-6 lg:gap-x-24">
+                                {
+                                    contactMethods.map((item, idx) => (
+                                        <li key={idx} className='border rounded-md p-4 bg-white'>
+                                            <div className="mt-3 flex items-center gap-x-3">
+                                                <div data-aos="fade-down" data-aos-duration="3000" className="flex-none text-blue-400 lg:p-4 p-2">
+                                                    {item.icon}
+                                                </div>
+                                                <div className='lg:p-0 p-2' data-aos="fade-right" data-aos-duration="400">
+                                                    <h4 className="lg:text-xl text-base text-blue-950 font-bold">{item.title}</h4>
+                                                    <p className='mt-2 lg:text-normal text-normal text-gray-600 '>{item.contact}</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="lg:w-1/2">
+                        <div data-aos="fade-right" data-aos-duration="300" className='lg:mt-0 mt-10'>
+                            <h1 className="font-bold text-lg text-gray-600 animate-slidein">
+                                Contactez-nous!
+                            </h1>
+                            <p className='mt-4 font-bold text-2xl text-blue-950'>
+                                Profitez d’une équipe d’experts très dynamique pour vous accompagner
+                            </p>
+                        </div>
+                        <div className="mt-12 max-w-xl" data-aos="fade-right" data-aos-duration="500">
+                            <form
+                                className="space-y-5"
+                            >
+                                <div className="flex flex-col items-center gap-y-5 gap-x-6 [&>*]:w-full sm:flex-row">
+                                    <div>
+                                        <label className="text-normal text-gray-600">
+                                            Prenom *
+                                        </label>
+                                        <input
+                                            type="text"
+                                            required
+                                            className="w-full bg-white mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-blue-600 shadow-sm rounded-lg"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label className="text-normal text-gray-600">
+                                            Nom *
+                                        </label>
+                                        <input
+                                            type="text"
+                                            required
+                                            className="w-full bg-white mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-blue-600 shadow-sm rounded-lg"
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <label className="text-normal text-gray-600">
+                                        Email *
+                                    </label>
+                                    <input
+                                        type="email"
+                                        required
+                                        className="w-full mt-2 px-3 py-2 bg-white text-gray-500 bg-transparent outline-none border focus:border-blue-600 shadow-sm rounded-lg"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="text-normal text-gray-600">
+                                        Telephone *
+                                    </label>
+                                    <input
+                                        type="tel"
+                                        required
+                                        className="w-full mt-2 px-3 py-2 bg-white text-gray-500 bg-transparent outline-none border focus:border-blue-600 shadow-sm rounded-lg"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="text-normal text-gray-600">
+                                        Message *
+                                    </label>
+                                    <textarea required className="w-full mt-2 bg-white h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-blue-600 shadow-sm rounded-lg"></textarea>
+                                </div>
+                                <button
+                                    className="flex px-10 py-6 text-white text-xl font-medium  bg-blue-600 hover:bg-blue-500 active:bg-blue-600 rounded-lg duration-150 shadow-3xl"
+                                    data-aos="fade-up"
+                                    data-aos-duration="3000"
+                                >
+                                    <span>Envoyer</span>
+                                    <MdArrowOutward className='h-7 w-7' />
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
         </div>
     )
 }
--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,30 +1,23 @@
 import React from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
+import { Ri24HoursLine } from "react-icons/ri";
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
+import { AiOutlineLike } from "react-icons/ai";
 function Features() {
     const features = [
         {
-            icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
-                </svg>,
+            icon: <Ri24HoursLine className='w-8 h-8'/>,
             title: "Support 24/7",
             desc: "Une équipe dédiée pour vous"
         },
         {
-            icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                </svg>,
+            icon: <AiOutlineSafetyCertificate className='w-8 h-8' />,
             title: "Satisfaction garantie",
             desc: "Une gestion de projet assurée"
         },
         {
-            icon:
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3" />
-                </svg>,
+            icon: <AiOutlineLike className='w-8 h-8' />,
             title: "Qualité de services",
             desc: "Un travail et une ergonomie au top"
         }
