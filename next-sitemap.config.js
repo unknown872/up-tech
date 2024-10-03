757cd164eff861b6532a96e24b26b8f7b2f4e79a
unknown872 <paulpogboom87@gmail.com>
2024-10-03 02:31:31 +0000

    Added XML file

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://uptechnologie-corpororation.com/', // Remplace par l'URL de ton site
+    generateRobotsTxt: true, // (optionnel) Génère également un fichier robots.txt
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
